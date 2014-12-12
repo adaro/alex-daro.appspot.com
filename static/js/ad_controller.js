764b1c1f7198be9c42082134eea556cbb93684ca
Alex Daro <alexdaro34@gmail.com>
2014-12-12 11:29:07 -0800

    Update ad_controller.js

--- static/js/ad_controller.js
+++ static/js/ad_controller.js
@@ -3,16 +3,6 @@
 //CONTROLLERS
 function ADController($scope, adServices) {
 
-    $scope.openRightMenu = function() {
-
-    };
-
-    var moviePromise = adServices.getMSData(); //use msServices to get csv data
-      moviePromise.then(function (data) {
-          $scope.movieData = data; // movieData
-      });
-
-
     $scope.submitForm = function(data, form) {
         console.log(data);
         if ( !form.$valid ) {
